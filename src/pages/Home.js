34ebbb3646cdc97efc3fce9bac0e3d274c0badac
panda3141592 <39628281+panda3141592@users.noreply.github.com>
2022-10-14 11:14:47 +0700

    routing

---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import Nav from "../components/Nav";
+
+const Home = () => {
+    const authToken = true;
+    const handleClick = () => {
+        console.log("clicked");
+    }
+
+    return(
+        <>
+            <Nav/>
+            <div className="home">
+                <h1>Swipe Right</h1>
+                <button className="primart-button" onClick={handleClick}>
+                    {authToken ? "Signout" : "Create Account"}
+                </button>
+            </div>
+        </>
+    );
+}
+
+export default Home;
---
+++ src/pages/Onboarding.js
@@ -0,0 +1,7 @@
+const Onboarding = () => {
+    return(
+        <div>onboarding</div>
+    )
+}
+
+export default Onboarding;
